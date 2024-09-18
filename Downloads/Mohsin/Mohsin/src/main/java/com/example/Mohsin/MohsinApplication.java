280dda57bc98b2ce05c6dff37917ba8dd8773831
Mohsin ali <mohsinali356070@gmail.com>
2024-09-18 16:20:30 +0530

    mohsin

---
+++ Downloads/Mohsin/Mohsin/src/main/java/com/example/Mohsin/MohsinApplication.java
@@ -0,0 +1,13 @@
+package com.example.Mohsin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MohsinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MohsinApplication.class, args);
+	}
+
+}
---
+++ Downloads/Mohsin/Mohsin/src/test/java/com/example/Mohsin/MohsinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Mohsin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MohsinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
